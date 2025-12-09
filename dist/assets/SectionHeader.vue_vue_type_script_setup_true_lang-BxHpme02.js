aa35f6e2d39e743eb31ef7804008237fddfbf135
roltexwp <resakia@digitalwinners.gr>
2025-12-09 18:55:04 +0400

    cors fix

---
+++ dist/assets/SectionHeader.vue_vue_type_script_setup_true_lang-BxHpme02.js
@@ -0,0 +1,6 @@
+import{c as a}from"./createLucideIcon-CBADZN3F.js";import{a as r,f as n,g as t,h as i,q as c,s as l,t as o,w as m,m as p,p as d,i as h,j as f}from"./index-BjBYBXDs.js";/**
+ * @license lucide-vue-next v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=a("arrow-right",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]),u={class:"flex items-center justify-between mb-8"},_={class:"text-2xl lg:text-3xl font-bold text-gray-900"},B=r({__name:"SectionHeader",props:{title:{},link:{}},setup(x){return(e,y)=>{const s=p("RouterLink");return t(),n("div",u,[i("h2",_,o(e.title),1),e.link?(t(),c(s,{key:0,to:e.link.to,class:"flex items-center text-primary-500 hover:text-primary-600 font-medium transition-colors"},{default:m(()=>[d(o(e.link.text)+" ",1),h(f(k),{class:"w-5 h-5 ml-1"})]),_:1},8,["to"])):l("",!0)])}}});export{B as _};
